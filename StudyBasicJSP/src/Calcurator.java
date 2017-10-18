5f642a4ea46fd2cae4523680dd5a4b0f50c49f39
USER <USER@USER19-PC>
2017-10-18 19:03:23 +0900

    곱셈함수 추가

--- StudyBasicJSP/src/Calcurator.java
+++ StudyBasicJSP/src/Calcurator.java
@@ -7,4 +7,8 @@ public class Calcurator {
 	public int substraction(int num1, int num2) {
 		return num1 - num2;
 	}
+	
+	public int multiplication(int num1, int num2) {
+		return num1 * num2;
+	}
 }
